ee359d22f1cda5c928f85eabd3fc154ec3436c90
Ian Apud <ianapud23@gmail.com>
2023-10-11 20:48:23 -0300

    casi terminado

--- js/index.js
+++ js/index.js
@@ -1,45 +1,75 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    const URL = "https://japceibal.github.io/japflix_api/movies-data.json" //url de json de peliculas
-    const listabusqueda = []
-
+    const URL = "https://japceibal.github.io/japflix_api/movies-data.json";
+    const listabusqueda = [];
+    let listafiltrado = [];
+    let boton = document.getElementById('btnBuscar');
+    let inputText = document.getElementById('inputBuscar'); // Mover la declaración aquí
+    let boxMovie = document.getElementById('lista')
     fetch(URL)
         .then(response => response.json())
         .then(responseData => {
             let data = responseData;
-            listabusqueda.push(data);
-            console.log(listabusqueda)
+            listabusqueda.push(...data);
         })
         .catch(error => console.log('Error:', error));
 
-    const btnsearch = document.getElementById('btnBuscar')
+    function buscarPorAtributos(elemento) {
+        const terminoBusqueda = inputText.value.toLowerCase(); // Mover la declaración aquí
+        return (
+            ( elemento.title.toLowerCase().includes(terminoBusqueda.toLowerCase())) ||
+            ( elemento.tagline.toLowerCase().includes(terminoBusqueda.toLowerCase())) ||
+            ( elemento.overview.toLowerCase().includes(terminoBusqueda.toLowerCase()))
+            );
+    }
 
-    btnsearch.addEventListener('click', () => {
-
-        const inputsearch = document.getElementById("inputBuscar").value;
-        const lista = document.getElementById("lista");
-        lista.innerHTML = '';
-
-        let busquedafiltrada = filtradobusqueda(listabusqueda, inputsearch)
-        console.log(busquedafiltrada)
-    });
-});
-
-function filtradobusqueda(lista, busqueda) {
-    return lista.filter(movies => {
-        return movies.some(movie => movie.genres[0].toLowerCase().includes(busqueda) || movie.tagline.toLowerCase().includes(busqueda) ||
-            movie.title.toLowerCase().includes(busqueda) || movie.overview.toLowerCase().includes(busqueda));
+    boton.addEventListener('click', () => {
+        if(inputText.value){
+        listafiltrado = listabusqueda.filter(buscarPorAtributos);
+        showCards()
+        console.log(listafiltrado);
+        }
     });
 
-}
-
-busqueda.addEventListener('input', () => { // Evento input para búsqueda
-    let inputtext = busqueda.value.trim().toLowerCase(); // Tomamos el valor de la búsqueda, lo convertimos a minúsculas y eliminamos espacios en blanco al principio y al final
-    if (inputtext === '') {
-        contenedor.innerHTML = ``;
-    } else {
-        let busquedafiltrada = filtradobusqueda(listabusqueda, inputtext); // Pasamos como argumentos la lista que tiene los datos de las categorías y también el valor del input de búsqueda
-        console.log(busquedafiltrada); // Mostramos en la consola todas las categorías que contienen productos filtrados
-        hojaBusqueda(busquedafiltrada);
+    function stars(rating) {
+        let starsHTML = ``;
+    
+        for (let i = 0; i < 5; i++) {
+            if (i < Math.trunc(rating / 2)) {
+                starsHTML += `<span class="fa fa-star checked"></span>`;
+            } else {
+                starsHTML += `<span class="fa fa-star"></span>`;
+            }
+        }
+    
+        return starsHTML;
+    }
+    
+    function showCards() {
+        boxMovie.innerHTML = ``;
+        listafiltrado.forEach(element => {
+            boxMovie.innerHTML += `<li> 
+                <div class="card bg-dark">
+                    <div class="card-header box-title leters">
+                    <h4> ${element.title} ${stars(element.vote_average)}</h4>
+                      <div class="dropdown">
+                        <button class="btn btn-dark left dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                         more...
+                         </button>
+                      <ul class="bg-dark  dropdown-menu">
+                       <li><a class="dropdown-item leters">Popularidad: ${element.popularity}</a></li>
+                        <li><a class="dropdown-item leters" >Estreno: ${element.release_date}</a></li>
+                        <li><a class="dropdown-item leters" >Duracion: ${element.runtime}</a></li>
+                      </ul>
+                </div>
+                    </div>
+                    <div class="card-body">
+                        <blockquote class="blockquote leters mb-0">
+                            <p>${element.overview}</p>
+                            <footer class="blockquote-footer">${element.tagline} <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
+                </div>
+            </div></li>`;
+        });
     }
 });
